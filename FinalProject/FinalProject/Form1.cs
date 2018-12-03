ecf1dc811b60989b1cbf7e0fca8dc1ae512b44f0
xxk3990 <xxk3990@IGM2435-28>
2018-12-03 12:47:56 -0500

    added hour time check

--- FinalProject/FinalProject/Form1.cs
+++ FinalProject/FinalProject/Form1.cs
@@ -12,6 +12,10 @@ namespace FinalProject
 {
     public partial class Form1 : Form
     {
+        string closed = " is closed.";
+        string open = " is open";
+        string longLine = " and the line is long. Just don't go.";
+        string shortLine = " and the line is short. GO!";
         public Form1()
         {
             InitializeComponent();
@@ -25,20 +29,29 @@ namespace FinalProject
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             String answer = comboBox1.Text;
-            foodAnswer.Text = answer;
+            var hourTime = new DateTime();
+            int hour = hourTime.Hour + 12;
+            //foodAnswer.Text = hour.ToString();
+
+            if (hour == 12 && answer == "Salsa's") //add restaurant name
+            {
+                foodAnswer.Text = answer + open + longLine;
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             var time = DateTime.Now.ToString("h:mm");
             label1.Text = time;
+            
           
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             
-            label1.Text = DateTime.Now.ToString("h:mm:ss");
+            label1.Text = DateTime.Now.ToString("h:mm");
         }
     }
 }
